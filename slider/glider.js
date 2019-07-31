a425c1a6b483f67302d336f3cf96b85a68a9e68d
Yohei Umezu <umeyouhei@gmail.com>
2019-07-31 12:54:46 +0900

    first commit

---
+++ slider/glider.js
@@ -0,0 +1,18 @@
+
+
+new Glider(document.querySelector('.glider'), {
+    slidesToShow: 4,
+    slidesToScroll: 14,
+    draggable: true,
+    dots: '#dots',
+    arrows: {
+        prev: '.glider-prev',
+        next: '.glider-next'
+    }
+});
+
+
+
+
+
+  
